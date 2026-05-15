1a0cbe6c34a2c859217108d7e143ea0974a7f067
Peace715 <peaceotoide78@gmail.com>
2026-05-15 07:34:28 +0100

    Delete payment-config.js

--- payment-config.js
+++
@@ -1,6 +0,0 @@
-window.VOGUEAURA_PAYMENT = {
-  // Replace this with your Paystack public key from Settings > API Keys & Webhooks.
-  // Use a pk_test... key while testing, then switch to pk_live... when you are ready.
-  paystackPublicKey: "YOUR_PAYSTACK_PUBLIC_KEY",
-  currency: "NGN"
-};
